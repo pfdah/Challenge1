69e9bf033c59ebb33f2ac55993240f4baaf829bb
pfdah <pratikdahal700@gmail.com>
2020-06-25 20:23:34 +0545

    What ajai milena

--- Game of chance.py
+++ Game of chance.py
@@ -6,18 +6,27 @@ num = random.randint(1,10)
 def head_tails(guess,amount):
     res = bool(random.randint(0,1))
     if res:
-        print('Heads')
-        return amount
+        print('The conin flipped Heads')
+        if guess == 'Heads':
+            print('You won')
+            return amount
+    print('The coin flipped Tails')
+    if guess == 'Tails':
+        print('You won')
+        return amount 
+    print('You lost')
     return -amount
-
 
 def roll_dice(guess,amount):
     dice1 = random.randint(1,6)
     dice2 = random.randint(1,6)
-    if (dice1 + dice2) % 2 == 0 and oddeven = "Even":
+    if (dice1 + dice2) % 2 == 0 and oddeven == "Even":
+        print('You won Dice Sum was Even')
         return amount 
-    elif (dice1 + dice2) % 2 != 0 and oddeven = "Odd":
+    elif (dice1 + dice2) % 2 != 0 and oddeven == "Odd":
+        print('You won Dice Sum was Odd')
         return amount
+    print('You lost Dice Sum was wrong')
     return -amount
 
 
@@ -31,19 +40,15 @@ def highroll(amount):
     if ycard in lst:
         ycard = random.randint(1,13)
     if ycard > ocard:
+        print('Your card was higher\nYou won')
         return amount
     elif ycard == ocard:
+        print('Your card was same\nTie')
         return 0
+    print('Your card was lower\nYou lost')
     return -amount
 
 
-'''def roulette(guess,amount):
-  rouletter = random.randint(0,36)
-  if rouletter == 0 and int(guess)  ! = 0:
-    return -amount
-  if rouletter == 
-
-'''
 def betcheck(amount):
     if amount > money:
         return False
@@ -53,37 +58,38 @@ def betcheck(amount):
 print('Welcome to Game Of Chance')
 while(True):
     print('What do you want to play?')
-    print('[1]. Coin Flip\n[2]. Cho-Han("Two dice sum")\n[3].Card Pick\n[4].Roulette\n[5]Quit\n>')
+    print('[1]. Coin Flip\n[2]. Cho-Han("Two dice sum")\n[3].Card Pick\n[4]Quit\n>')
     choice = int(input(':\n>'))
     if choice == 1:
         print('You have',str(money)' points')
         print('Place your bet')
         bet = input(str(money))
           if betcheck:
-              money += head_tails(bet)
+                Guess = input('Enter your guess (\'Heads\') or (\'Tails\')').title
+                money += head_tails(Guess,bet)
          else:
-             print('You bad boy')
-             continue
+                print('You bad boy')
+                continue
     elif choice == 2:
         print('You have',str(money)' points')
         print('Place your bet')
         bet = input(str(money))
-          if betcheck:
-              guess = input('Guess Wether the result of Rolls will be (Odd) or (Even)').title()
-              money += roll_dice(guess,bet)
-         else:
-             print('You bad boy')
-             continue
+        if betcheck:
+            guess = input('Guess Wether the result of Rolls will be (Odd) or (Even)').title()
+            money += roll_dice(guess,bet)
+        else:
+            print('You bad boy')
+            continue
     elif choice == 3:
-        print('You have',str(money)' points')
+        print('You have',str(money),' points')
         print('Place your bet')
         bet = input(str(money))
-          if betcheck:
-              money += roll_dice(bet)
-         else:
-             print('You bad boy')
-             continue
-    elif choice == 5:
+        if betcheck:
+            money += roll_dice(bet)
+        else:
+            print('You bad boy')
+            continue
+    elif choice == 4:
         print('Bye Human')
         exit()
     else:
